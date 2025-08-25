e7f3a05f4c14790aa6467ea9e561711977737223
Idris Raja <idris.raja@gmail.com>
2025-08-25 11:10:23 -0500

    trash out the old code

--- code/01.tf
+++ code/01.tf
@@ -1,15 +1,6 @@
-# Define a Terraform module that accepts a region name as input. It should then output both:
-# The region name in all uppercase.
-# The number of characters in the region string.
+provider "local" {}
 
-variable "region" {
-  type = string
-}
-
-output "upper" {
-  value = upper(var.region)
-}
-
-output "length" {
-  value = length(var.region)
+resource "local_file" "hello" {
+  content  = "hello terraform"
+filename = "${path.module}/hello.txt"
 }
